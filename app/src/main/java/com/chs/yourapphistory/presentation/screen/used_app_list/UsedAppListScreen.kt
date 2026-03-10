d9bf10009f7358de310e8734332d31bca89c9dcc
HeonSeokCha <hs960829@gmail.com>
2026-03-10 21:06:58 +0900

    fix display last bottom item.

--- app/src/main/java/com/chs/yourapphistory/presentation/screen/used_app_list/UsedAppListScreen.kt
+++ app/src/main/java/com/chs/yourapphistory/presentation/screen/used_app_list/UsedAppListScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,6 +15,7 @@ import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
@@ -32,13 +34,17 @@ import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.dropShadow
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.shadow.Shadow
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -129,7 +135,8 @@ fun UsedAppListScreenScreen(
 
             LazyColumn(
                 modifier = Modifier.fillMaxSize(),
-                horizontalAlignment = Alignment.CenterHorizontally
+                horizontalAlignment = Alignment.CenterHorizontally,
+                contentPadding = PaddingValues(bottom = 64.dp)
             ) {
                 if (state.isLoading) {
                     items(20) {
@@ -193,7 +200,16 @@ private fun AppSearchBar(
                     .fillMaxWidth()
                     .height(48.dp)
                     .padding(horizontal = 8.dp)
-                    .background(color = Color.White, RoundedCornerShape(50)),
+                    .background(color = Color.White, RoundedCornerShape(50))
+                    .dropShadow(
+                        shape = RoundedCornerShape(20.dp),
+                        shadow = Shadow(
+                            radius = 10.dp,
+                            spread = 1.dp,
+                            color = Color(0x1A000000),
+                            offset = DpOffset(x = 4.dp, 4.dp)
+                        )
+                    ),
                 verticalAlignment = Alignment.CenterVertically
             ) {
                 Spacer(modifier = Modifier.width(8.dp))
